3827536a74cccdd32950b4c0569be6315aa18a4c
charlotte de belay <charlotte.debelay@ehl.ch>
2021-06-15 16:33:58 +0200

    Updated app + Baseline model

--- app.py
+++ app.py
@@ -1,1 +1,22 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+from PIL import Image
+
+# image
+image = Image.open('logo.jpg')
+st.image(image, width=100, use_column_width=None)
+
+# title
+st.markdown("""# MUSH ME
+## You went mushroom picking and you wonder if you can eat a mushroom?
+Upload your picture and check it by yourself! """)
+
+# file uploader
+uploaded_file = st.file_uploader("Choose a file", accept_multiple_files=True)
+
+# image
+image = Image.open('mushroom.jpg')
+st.image(image)
+
 
