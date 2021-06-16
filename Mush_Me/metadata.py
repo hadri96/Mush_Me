d102524c23fcb9350f2b1f1c6d02d9fe36bd1d76
Hadrien Morand <hadrien.morand@ehl.ch>
2021-06-16 17:58:08 +0200

    updated metadata.py & utils.py

--- Mush_Me/metadata.py
+++ Mush_Me/metadata.py
@@ -3,17 +3,24 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import os
+import shutil
+from Mush_Me.utils import log_progress
 
 class Data:
     def __init__(self, _set='mini_train'):
 
         ### _set should be the set you'd like to have
-        ### You can pick between 4 datasets = 'train', 'test', 'mini_train', 'mini_test'
-        local_path_to_mush = '/'.join(os.getcwd().split('/')[:6])
-        self._set = _set
-        self.path = os.path.join(local_path_to_mush, 'Mush_Me', 'data',
-                                 'raw_data', self._set)
-        self.path_to_metadata = os.path.join(local_path_to_mush, 'Mush_Me', 'data', 'metadata',
+        ### You can pick between 4 datasets = 'train', 'test', 'mini_train', 'mini_test', 'all'
+        self.local_path_to_data = os.path.join('/'.join(os.getcwd().split('/')[:6]),'Mush_Me','data')
+        if _set in ['train', 'test']:
+            answer = input('You have selected a big dataset, are you sure you\'d like to proceed? \
+                            (This might present some limitations further) [y/n]')
+            if answer == 'y':
+                self._set = _set
+            else:
+              self._set = input('Please enter another set [mini_train, mini_test]')
+        self.path = os.path.join(self.local_path_to_data,'raw_data', self._set)
+        self.path_to_metadata = os.path.join(self.local_path_to_data, 'metadata','01_initial_metadata',
                                              ('DanishFungi2020_' + _set + '_metadata.csv'))
 
     def get_metadata(self):
@@ -21,9 +28,11 @@ class Data:
         self.len_initial = len(data)
         return data
 
+
     def get_clean_metadata(self, columns=False):
 
-        ### Filtering the data based on the kingdom Fungi, CoorUncert small than 1000m and collected in 2000 or later
+        ### Filtering the data based on the kingdom Fungi, CoorUncert small than 1000m and collected in 2000 or late
+
         data = self.get_metadata()
 
         data_filtered = data[data['kingdom']=='Fungi']\
@@ -58,6 +67,8 @@ class Data:
 
         data_filtered.reset_index(inplace=True)
 
+        data_filtered.drop(columns=['level_0', 'index'], inplace=True)
+
         ### Select specific columns
 
         if columns:
@@ -65,8 +76,13 @@ class Data:
             num_columns = int(input('How many columns would like to keep? '))
             cols = []
             for _ in range(num_columns):
-                  col = input('Give a column name: ')
-                  cols.append(col)
+                col = input('Give a column name: ')
+                if col == 'image_path':
+                    print('This column is included by default, no need to input it!')
+                    continue
+                cols.append(col)
+
+            cols.append('image_path')
 
             data_filtered = data_filtered[cols]
 
@@ -75,3 +91,81 @@ class Data:
 
         return self.clean_metadata
 
+    def create_csv(self, new_directory = False):
+        self.path_clean_metadata = os.path.join(self.local_path_to_data,
+                                                'metadata',
+                                                '02_cleaned_metadata')
+
+        number_of_files = len(os.listdir(self.path_clean_metadata))
+
+        if new_directory:
+
+            version = 'v'+str(number_of_files+1)
+
+            path_new_dir = os.path.join(self.path_clean_metadata,version)
+
+            os.mkdir(path_new_dir)
+
+            pd.to_csv(path_new_dir+self._set+'_'+version)
+
+        else:
+
+            version = 'v'+str(number_of_files)
+
+            path_dir = os.path.join(self.path_clean_metadata, version)
+
+            pd.to_csv(path_dir + self._set + '_' + version)
+
+        return 'CSV created successfully'
+
+    def select_images(self, create_new_directory=True):
+
+        data = self.get_clean_metadata()
+
+        image_paths = list(data['image_path'])
+
+        print('Be careful, you should only run this function once!')
+
+        if self._set in ['train','test']:
+            print('Do not run this method with a full dataset')
+            return None
+
+        path_to_images = os.path.join(self.local_path_to_data, 'raw_data','pictures')
+
+        number_of_files = len(os.listdir(path_to_images))
+
+        if create_new_directory:
+
+            version = f'0{str(number_of_files)}_Dataset'
+
+            path_new_dir = os.path.join(path_to_images,version,self._set)
+
+            os.make_dir(path_new_dir)
+
+            for path in log_progress(image_paths, every=1):
+
+                origin = os.path.join(path_to_images, '00_initial_set',self._set, path)
+
+                target = os.path.join(path_new_dir, path)
+
+                shutil.copy2(origin,target)
+
+            return 'Transfer successfully executed!'
+
+        print(os.listdir(path_to_images))
+
+        directory = input('Please select in which directory you\'d like to insert this new file:')
+
+        path_dir = os.path.join(path_to_images,directory,self._set)
+
+        os.mkdir(path_dir)
+
+        for path in log_progress(image_paths, every=1):
+
+            origin = os.path.join(path_to_images, '00_initial_set', path)
+
+            target = os.path.join(path_dir, path)
+
+            shutil.copy2(origin,target)
+
+        return 'Transfer successfully executed'
